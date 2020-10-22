4af32f5161c5146b215a37e960413fad8bbd13fe
Farooq Mahmud <farooq@teqniqly.com>
2020-10-22 16:24:02 -0700

    Console app

---
+++ PowerBiBuddy/Client/PbiClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PowerBiBuddy
+{
+    public class PbiClient : IDisposable
+    {
+        private bool disposed = false;
+        private readonly HttpClient httpClient;
+
+        public PbiClient(string userAgent, string authToken)
+        {
+            this.httpClient = new HttpClient {BaseAddress = new Uri("https://api.powerbi.com/v1.0/myorg/")};
+            this.httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+            this.httpClient.DefaultRequestHeaders.Add("Authorization", authToken);
+        }
+
+        public async Task<string> GetWorkspacesAsync()
+        {
+            var response = await this.httpClient.GetAsync("groups");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+
+            var jo = JObject.Parse(json);
+
+            return "foo";
+        }
+
+        public void Dispose() => Dispose(true);
+
+        private void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.httpClient.Dispose();
+            }
+
+            this.disposed = true;
+        }
+    }
+}
---
+++ PowerBiBuddy/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace PowerBiBuddy
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var token = await GetTokenAsync();
+            var client = new PbiClient("power-bi-buddy-console", token);
+
+            using (client)
+            {
+                var workspaces = await client.GetWorkspacesAsync();
+                Console.WriteLine(workspaces);
+            }
+
+            Console.Read();
+        }
+
+        private static async Task<string> GetTokenAsync()
+        {
+            var authResult = await LoginInteractive();
+            var token = authResult.CreateAuthorizationHeader();
+            return token;
+        }
+
+        private static async Task<AuthenticationResult> LoginInteractive()
+        {
+            var authContext = new AuthenticationContext("https://login.windows.net/common/oauth2/authorize");
+
+            var authResult = await authContext.AcquireTokenAsync(
+                "https://analysis.windows.net/powerbi/api",
+                "3120e4df-4b0d-4be9-b117-dc835aa84646",
+                new Uri("https://dev.powerbi.com/Apps/SignInRedirect"),
+                new PlatformParameters(PromptBehavior.Always));
+
+            return authResult;
+        }
+
+        private static HttpClient CreateHttpClient(string authToken)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", authToken);
+            return client;
+        }
+    }
+}
